a4563c370124c84f89e2d356d6de771a21050533
agnieszka <the.larch19@gmail.com>
2020-06-23 13:29:47 +0200

    Setting up

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -15,12 +15,20 @@
 
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
-usethis::use_package( "thinkr" )
+usethis::use_package( "ggplot2" )
+usethis::use_package( "dplyr" )
+usethis::use_package( "agricolae" )
+usethis::use_package( "tidyr" )
+usethis::use_package( "readxl" )
+usethis::use_package( "ggpubr" )
+usethis::use_package( "stringr" )
+usethis::use_package( "ggbeeswarm" )
+usethis::use_package( "magrittr" )
 
 ## Add modules ----
 ## Create a module infrastructure in R/
-golem::add_module( name = "name_of_module1" ) # Name of the module
-golem::add_module( name = "name_of_module2" ) # Name of the module
+## golem::add_module( name = "name_of_module1" ) # Name of the module
+## golem::add_module( name = "name_of_module2" ) # Name of the module
 
 ## Add helper functions ----
 ## Creates ftc_* and utils_*
